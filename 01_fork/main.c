6c29c874171909ba45f9f05f77658a7bf18d9f85
tim <15291418231@163.com>
2018-09-18 07:53:46 -0700

    my first commit!

---
+++ 01_fork/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+int main() {
+    pid_t ret = fork();
+    if(ret<0) {
+        printf("error\n");
+    } else if(ret == 0){
+        while (1){
+            printf("child: pid=%d ppid=%d\n",getpid(),getppid());
+            sleep(1);
+        }
+    } else{
+        while(1){
+            printf("parent: pid=%d ppid=%d\n",getpid(),getppid());
+            sleep(1);
+        }
+    }
+
+    return 0;
+}
