ab1784f7df0836a96ef1517b1b6aa2d24c2ad3a8
zouchanglin <15291418231@163.com>
2018-09-21 23:11:42 +0800

    add Progress bar(add color change)!

---
+++ 02_OpencvDemo/cmake-build-debug/CMakeFiles/02_OpencvDemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/02_OpencvDemo.dir/main.cpp.o"
+  "02_OpencvDemo.pdb"
+  "02_OpencvDemo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/02_OpencvDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 03_Progress bar/main.c
@@ -0,0 +1,43 @@
+/*************************************************************************
+	> File Name: main.c
+	> Author: Tim
+	> Mail: 15291418231@163.com
+	> Created Time: 2018年09月21日 星期五 03时23分51秒
+ ************************************************************************/
+
+#include<unistd.h>
+#include<stdio.h>
+
+int main(int argc, char *argv[])
+{
+    char buf[102] = {0};
+    int i = 0;
+    //注意这里需要使用转义字符
+    char arr[4] = "/-|\\";
+    for(i= 0;i<=100;i++)
+    {
+        buf[i] = '#';
+        if(i<40)
+        {
+            //进度条颜色修饰
+            printf("[\033[0;31m%-100s\033[0;39m] [%d%%][%c]\r",buf,i,arr[i%4]);
+        }
+        else if(i>=40 && i< 80)
+        {
+            printf("[\033[0;33m%-100s\033[0;39m] [%d%%][%c]\r",buf,i,arr[i%4]);
+        }
+        else
+        {
+            printf("[\033[0;32m%-100s\033[0;39m] [%d%%][%c]\r",buf,i,arr[i%4]);
+        }
+        //printf("[%-100s] [%d%%][%c]\r",buf,i,arr[i%4]);
+
+        //printf是按行缓冲的，所以需要刷新缓冲区
+        fflush(stdout);
+        //usleep的单位是微秒
+        usleep(50000);
+    }
+    //最后需要换行
+    printf("\n");
+    return 0;
+}
