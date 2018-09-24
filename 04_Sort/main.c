ba67c56a3128e3dec46f42b20dbe8da02ab5067f
zouchanglin <15291418231@163.com>
2018-09-24 16:21:13 +0800

    Add Sort!

---
+++ 04_Sort/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <stdlib.h>
+
+void PrintArray(const int *arr,int n)
+{
+    int i = 0;
+    assert(arr);
+    for(i=0; i<n; i++)
+        printf("%d ",arr[i]);
+    printf("\n");
+}
+
+//插入排序
+void InsertSort(int *arr, int n)
+{
+    int index = 0;
+    assert(arr);
+    for(index=0;index<n;index++)
+    {
+        //将当前数据往前插入
+        int end = index-1;
+        int temp = arr[index];
+        while(end>=0 && temp<arr[end])
+        {
+            arr[end+1] = arr[end];
+            --end;
+        }
+
+        arr[end+1] = temp;
+    }
+}
+
+//希尔排序
+void ShellSort(int* arr,int len)
+{
+    int gap = len;
+    while(gap>1)
+    {
+        gap = gap/3+1;
+        int cur = 0;
+        for(cur = gap;cur<len;++cur)
+        {
+            int end = cur-gap;
+            int tmp = arr[cur];
+            while(end>=0 && tmp<arr[end])
+            {
+                arr[end+gap] = arr[end];
+                end -= gap;
+            }
+            arr[end+gap] = tmp;
+        }
+    }
+}
+
+
+
+int main(int argc, char* argv[]) {
+    int array[]={4,6,8,3,5,11,19,4,2,6,1};
+    int len = sizeof(array)/sizeof(int);
+    PrintArray(array,len);
+    //InsertSort(array,len);
+    ShellSort(array,len);
+    PrintArray(array,len);
+    return 0;
+}
