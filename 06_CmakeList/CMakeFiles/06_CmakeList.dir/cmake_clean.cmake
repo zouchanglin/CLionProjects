47f9b630de1a5c078d13ab96bbded8a2dad55019
zouchanglin <15291418231@163.com>
2018-09-28 16:20:50 +0800

    add calc.html

---
+++ 06_CmakeList/CMakeFiles/06_CmakeList.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/06_CmakeList.dir/main.cpp.o"
+  "06_CmakeList.pdb"
+  "06_CmakeList"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/06_CmakeList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 06_CmakeList/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <unistd.h>
+
+using namespace std;
+using namespace cv;
+
+int add(int a, int b){
+    return a+b;
+}
+int sub(int a,int b,int c){
+    return a+b+c;
+}
+int main() {
+    Mat src = imread("/home/zcl/图片/timg.jpeg");
+    namedWindow("Hello",CV_WINDOW_AUTOSIZE);
+    
+    imshow("Hello",src);
+    waitKey(0);
+
+
+
+    return 0;
+}
