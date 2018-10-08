5155d075ae06bb6bb10eda63aa171a6ddaef16c8
zouchanglin <15291418231@163.com>
2018-10-07 22:40:20 -0400

    delete eclipsewk

--- eclipsework/20180915/src/com/xpu/demo/Demo.java
+++
@@ -1,9 +0,0 @@
-package com.xpu.demo;
-//IDEA测试项目
-public class Demo {
-    public static void main(String[] args) {
-        for (int i = 0; i <10 ; i++) {
-            System.out.println("out");
-        }
-    }
-}
--- eclipsework/Register/src/com/xpu/utils/WriteExcel.java
+++
@@ -1,5 +0,0 @@
-package com.xpu.utils;
-
-public class WriteExcel {
-
-}
